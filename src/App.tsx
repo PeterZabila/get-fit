6b2262ccd6769104dfa3bdafc3749f51fec76269
PeterZabila <petrozabila@gmail.com>
2023-10-11 17:50:24 +0300

    1st base ts code

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Navbar } from "@/scenes/navbar";
+import { SelectedPage } from '@/shared/types';
+
+
+function App() {
+const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  return (
+    <div className="">
+      <Navbar
+         selectedPage={selectedPage}
+         setSelectedPage={setSelectedPage}
+      />
+    </div>
+  )
+}
+
+export default App
